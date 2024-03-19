5553c9778109fb33f1dabc39dbcf388efc5d83ab
Egemen Agustos <info@egemenagustos.com>
2024-03-19 22:18:59 +0300

    Create project layers

---
+++ CleanArchitecture.Domain/Entities/Brand.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistance.Repositories;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class Brand : Entity<Guid>
+    {
+        public Brand(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Brand()
+        {
+        }
+
+        public string Name { get; set; }
+    }
+}
