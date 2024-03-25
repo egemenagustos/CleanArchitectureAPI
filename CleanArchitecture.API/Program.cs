41fabb511ad36db1194bfbe8845b1d1c758c76cf
Egemen Agustos <info@egemenagustos.com>
2024-03-25 22:01:58 +0300

    exception-logging-implementation

--- CleanArchitecture.API/Program.cs
+++ CleanArchitecture.API/Program.cs
@@ -35,7 +35,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 //    app.ConfigureCustomExceptionMiddleware();
 //}
 
-//app.ConfigureCustomExceptionMiddleware();
+app.ConfigureCustomExceptionMiddleware();
 
 
 
